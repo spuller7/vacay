ba4cf7d29ee37597eef6478b295f0ca0c8d8171e
Travis Spuller <spuller7@gmail.com>
2021-08-02 12:01:46 -0400

    Update AdventureController.php

--- app/controllers/AdventureController.php
+++ app/controllers/AdventureController.php
@@ -37,7 +37,7 @@ class AdventureController extends Controller {
         
         $ajax = new AjaxResponse();
         $key = 'S1W-ieZyern54fLk4CPDCu87ugxJ0rE4YAVmryZHwgQ';
-        $ajax->response  = json_decode(file_get_contents('https://discover.search.hereapi.com/v1/discover?in=circle:42.2808,-83.7430;r=30000&q=restaurants&apiKey='.$key), true);
+        $ajax->response  = json_decode(file_get_contents('https://discover.search.hereapi.com/v1/discover?in=circle:42.2808,-83.7430;r=30000&q=restaurants&limit=1000&apiKey='.$key), true);
         $ajax->success = true;
 
         $ajax->send();
