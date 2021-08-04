2ec6a0b0d56901dc4f8b6ce7954cb96f2367ad43
tspuller <travis.spuller@gmail.com>
2021-08-04 01:52:28 -0400

    search

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -1,5 +1,5 @@
-
 <?php
+
 namespace app\controllers;
 
 use app\core\Application;
@@ -38,6 +38,21 @@ class SearchController extends Controller {
         $ajax = new AjaxResponse();
         $key = 'S1W-ieZyern54fLk4CPDCu87ugxJ0rE4YAVmryZHwgQ';
         $ajax->response  = json_decode(file_get_contents('https://discover.search.hereapi.com/v1/discover?in=circle:42.2808,-83.7430;r=30000&q=restaurants&limit=100&apiKey='.$key), true);
+        $ajax->success = true;
+
+        $ajax->send();
+    }
+
+    public function search_suggestions(Request $request, Response $response)
+    {
+        
+        $ajax = new AjaxResponse();
+        error_log(print_r($_GET, true));
+        $query = json_encode($_GET['query']);
+        $key = 'AIzaSyA3tAENcwKmOa6m2Y4B4SIXbEEi_GN0F4A';
+        $request_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='.$query.'&inputtype=textquery&fields=formatted_address,name&locationbias=circle:2000@42.2808,-83.7430&key='.$key;
+        $ajax->query = $_GET['query'];
+        $ajax->response  = json_decode(file_get_contents($request_url), true);
         $ajax->success = true;
 
         $ajax->send();
---
+++ app/views/search.php
@@ -0,0 +1,236 @@
+<div id="searchPage" class="background">
+    <div class="container" style="margin: auto;">
+
+        <div class="row">
+            <div class="autocomplete" style="display:inline-flex; margin-left:auto;">
+                <input id="commemorateInput" type="text" style="width:20vw;" class="form-control" name="" value='' placeholder='Search For Your Favorite Spots'>
+
+                <div></div>
+
+                <button style="white-space: nowrap;">Commemorate Adventure</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="card" style="width: 100%">
+
+                <div class="card-body">
+                    <div id="search" class="container">
+                        <form id="search-examples-form">
+                            <div class="row" style="align-items: flex-end;">
+                                <div class="col-9">
+                                    <label for="filter1">City</label>
+                                    <input type="text" class="form-control" name="filter1" value='Ann Arbor, Michigan' disabled>
+                                </div>
+                                <div class="col-3">
+                                    <button id="exploreButton" class="btn btn-labeled" style="height: calc(1.5em + .75rem + 2px); background: linear-gradient(135deg,#ff690f 0%,#e8381b 100%); color: #ffffff; width: 100%;">Explore</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="filters">Filters</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <label>Categories</label>
+                                    <div style="display: block;">
+                                        <button class="btn btn-primary">Breakfast</button>
+                                        <button class="btn btn-primary">Lunch</button>
+                                        <button class="btn btn-primary">Dinner</button>
+                                        <button class="btn btn-primary">Coffee & Tea</button>
+                                        <button class="btn btn-primary">Sweets</button>
+                                        <button class="btn btn-primary">21+</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <label>Price</label>
+                                    <div style="display: block;">
+                                        <button class="btn btn-primary">Free</button>
+                                        <button class="btn btn-primary">$</button>
+                                        <button class="btn btn-primary">$$</button>
+                                        <button class="btn btn-primary">$$$</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div id="result" class="container" style="display: none;">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1>{{location['title']}}</h1>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="infoHeader">Address</div>
+                                <p>{{location['address']}}</p>
+                            </div>
+
+                            <div class="col-6">
+                                <div class="infoHeader">Hours</div>
+                                <p>{{location['hours']}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+
+/**
+ * Page Dynamic Controller
+ * 
+ * Using view object so that whenever the items property is updated,
+ * the table will automatically be updated without the need to listen
+ * for any data changes. However, this is just a preference, can be done
+ * with only javascript if desired
+ */
+var searchController = new Vue({
+    el: '#searchPage',
+    data: {
+        location: {
+            'title': 'test title',
+            'address': 'test address',
+            'hours': 'hours'
+        },
+    },
+    methods: {
+        getAdventure: function() {
+            let data = {};
+            let controller = this;
+
+            $.ajax({
+                // url directed to a the getExamplesTable function in the datatable.php in /contollers
+                url: "/adventure/discover",
+                type: 'GET',
+                data: data,
+                success: function(res)
+                {
+                    if (res.success)
+                    {
+                        let numResults = res.response.items.length;
+
+                        let choiceIndex = Math.floor(Math.random() * numResults);
+                        let adventure = res.response.items[choiceIndex];
+                        console.log(adventure);
+                        searchController.location['title'] = adventure['title'];
+                        searchController.location['hours'] = adventure['openingHours'] ? adventure['openingHours'][0]['text'][0] : 'N/A';
+                        searchController.location['address'] = adventure['address']['label'];
+
+                        $('#search').hide();
+                        $('#result').show();
+                    }
+                    else
+                    {
+                        console.log("Unexpected Error");
+                    }
+                }
+
+            });
+        },
+
+        getPlaceSuggestions: function(input)
+        {
+            let data = {};
+            data['query'] = input;
+
+            $.ajax({
+                // url directed to a the getExamplesTable function in the datatable.php in /contollers
+                url: "/search/search_suggestions",
+                type: 'GET',
+                data: data,
+                success: function(res)
+                {
+                    if (res.success)
+                    {
+                        let a, b, i;
+                        let val = res.query;
+                        closeAllLists();
+                        if (!res.response.candidates) { return false;}
+
+                        let suggestions = res.response.candidates;
+                        console.log(suggestions);
+                        if (suggestions.length <= 0) { return false;}
+
+                        currentFocus = -1;
+                        /*create a DIV element that will contain the items (values):*/
+                        a = document.createElement("DIV");
+                        a.setAttribute("class", "autocomplete-items");
+                        /*append the DIV element as a child of the autocomplete container:*/
+                        document.getElementById('commemorateInput').parentNode.appendChild(a);
+
+                        for (i = 0; i < suggestions.length; i++) {
+                                /*create a DIV element for each matching element:*/
+                                b = document.createElement("DIV");
+                                /*make the matching letters bold:*/
+                                b.innerHTML = "<p>" + suggestions[i].name + ' - ' + suggestions[i].formatted_address + "</strong>";
+                                //b.innerHTML += arr[i].substr(val.length);
+                                /*insert a input field that will hold the current array item's value:*/
+                                console.log(b);
+                                b.innerHTML += "<input type='hidden' value='" + suggestions[i].name + "'>";
+                                /*execute a function when someone clicks on the item value (DIV element):*/
+                                b.addEventListener("click", function(e) {
+                                    /*insert the value for the autocomplete text field:*/
+                                    $('#commemorateInput').value = this.getElementsByTagName("input")[0].value;
+                                    /*close the list of autocompleted values,
+                                    (or any other open lists of autocompleted values:*/
+                                    closeAllLists();
+                                });
+                                a.appendChild(b);
+                        }
+                    }
+                    else
+                    {
+                        console.log("Unexpected Error");
+                    }
+                }
+
+            });
+        }
+    }
+});
+
+// Jquery event handler functions
+$(function() {
+    $('#search-examples-button').on('click', function() {
+        $('#search-examples-form').submit();
+    });
+
+    $('#commemorateInput').on('input', function() {
+        if (this.value.length > 2)
+        {
+            searchController.getPlaceSuggestions(this.value);
+        }
+    });
+
+    $('#exploreButton').on('click', function(e) {
+        e.preventDefault();
+        searchController.getAdventure();
+    });
+
+    
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+});
+
+function closeAllLists(elmnt) {
+    let x = $(".autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i]) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+</script>
